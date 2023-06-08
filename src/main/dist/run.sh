906b45c34b13aea9c9249b8bf37be8bca8f784f6
llamersmcw <llamers@mcw.edu>
2023-06-08 10:49:49 -0500

    Update run.sh

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Eva loading pipeline
+# Rat Mutation Sequence Pipeline
 #
 . /etc/profile
 APPNAME=rat-mutation-sequences-pipeline
@@ -9,8 +9,13 @@ SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
 
+EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,sjwang@mcw.edu
+fi
+
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar"$@" > run.log 2>&1
 
-mailx -s "[$SERVER] Rat Mutation Sequence Pipeline Run" mtutaj@mcw.edu,llamers@mcw.edu < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] Rat Mutation Sequence Pipeline Run" $EMAILLIST < $APPDIR/logs/summary.log
